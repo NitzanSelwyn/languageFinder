04e9a84f70daf7534bb59e4a9f225a0192c536e4
nitzan selwyn <nitzansel@gmail.com>
2019-07-13 22:51:13 +0300

    the program

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string html = string.Empty;
+            string url = @"https://api.ipdata.co?api-key=";
+            JObject jObject;
+
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+                jObject = JObject.Parse(html);
+            }
+
+
+            Console.WriteLine(jObject["languages"][0]["name"]);
+
+            Console.Read();
+        }
+
+    }
+}
